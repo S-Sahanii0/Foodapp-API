747b682b6a2f30c328cb29de0fc67823321fa97a
S-Sahanii0 <55910981+S-Sahanii0@users.noreply.github.com>
2021-04-12 18:58:47 +0545

    Added files for deployment

--- FoodAPI/settings.py
+++ FoodAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-4!16hj%7u2&_(5dut$(b&lkq-w56#@_ff1z-wsd*fmkywmbe4q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://restura-backend.herokuapp.com/']
 
 
 # Application definition
@@ -135,3 +136,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
--- account/admin.py
+++ account/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from account.models import Account
 
-admin.site.site_header = "INTERNALAYA | Internships and Job Opportunities"
+admin.site.site_header = "Foodapp API"
 
 class AccountAdmin(UserAdmin):
 	list_display = ('email','firstname','lastname','date_joined', 'last_login', 'is_admin', 'is_staff')
